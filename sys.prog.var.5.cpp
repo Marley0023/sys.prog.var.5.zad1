f5b0556e14c16766a8cae558a1c4ada25f26edc9
Honor <Honor@ENROXES-L>
2024-11-22 22:06:41 +0500

    Добавьте файлы проекта.

---
+++ sys.prog.var.5.cpp
@@ -0,0 +1,17 @@
+﻿
+using namespace std;
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#define t 3
+#define z (1 / tan(t)+2)
+#define y 4
+#define x 8*z/(exp(t) + 2)-pow(y, 2)
+
+int main()
+{
+	printf("%f\t\n", x);
+	return 0;
+}
+
